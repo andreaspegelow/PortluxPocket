33141a7e111e398a3f19ec2e3cf96e2126fbceda
andreaspegelow <andpegelow@gmail.com>
2015-06-17 01:50:49 +0200

    Basic layout

--- PortluxPocket/app/src/main/java/com/portlux/portluxpocket/SearchActivity.java
+++ PortluxPocket/app/src/main/java/com/portlux/portluxpocket/SearchActivity.java
@@ -1,38 +1,60 @@
 package com.portlux.portluxpocket;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class SearchActivity extends ActionBarActivity {
+public class SearchActivity extends Activity implements TextWatcher {
+    EditText searchField;
+    SearchModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        searchField = (EditText)findViewById(R.id.searchField);
+        searchField.addTextChangedListener(this);
+
+        //Create the model
+        model = new SearchModel();
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_search, menu);
+
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        model.search(searchField.getText().toString());
+        updateList();
+    }
+
+    private void updateList() {
+
     }
 }
