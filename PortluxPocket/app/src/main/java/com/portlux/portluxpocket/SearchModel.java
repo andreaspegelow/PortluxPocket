c8a95ca6c6bebc9e7ab3cd2b124cbae2f82819f1
andreaspegelow <andpegelow@gmail.com>
2015-07-15 00:10:05 +0200

    Sorting the search result

--- PortluxPocket/app/src/main/java/com/portlux/portluxpocket/SearchModel.java
+++ PortluxPocket/app/src/main/java/com/portlux/portluxpocket/SearchModel.java
@@ -72,6 +72,17 @@ public class SearchModel {
             }
         }
 
+
+
+        //Sorting depending on wich mode you are in (not done yet)
+        Collections.sort(searchResult, new Comparator<User>() {
+            @Override
+            public int compare(User user1, User user2) {
+
+                return user1.getName().compareTo(user2.getName());
+            }
+        });
+
         return searchResult;
     }
 
